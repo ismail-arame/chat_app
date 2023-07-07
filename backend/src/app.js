e046f6e95b0a7222057c86fc21cfab9cad56f9de
ismail-arame <ismail.arame66@gmail.com>
2023-07-07 11:09:21 +0100

    added some middleware to our server to increase security and functionnality

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,49 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+
+//dot env config
+dotenv.config();
 
 const app = express();
 
+//Morgan
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+//Helmet
+app.use(helmet());
+
+//parse json from body and url middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//sanitize mongo request data
+app.use(mongoSanitize());
 app.get("/", (req, res) => {
-  res.send("hello from the end of the world");
+  res.status(200).send("hello from the end of the world");
 });
+
+//cookie parser
+app.use(cookieParser());
+
+//gzip compression
+app.use(compression());
+
+//file upload
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+app.use(cors());
 
 module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,8 @@
 const app = require("./app");
-const dotenv = require("dotenv");
-
-//dot env config
-dotenv.config();
 
 //env variables
 const PORT = process.env.PORT || 8000;
+console.log(process.env.NODE_ENV);
 
 app.listen(PORT, () => {
   console.log("Server is listening on port ", PORT);
