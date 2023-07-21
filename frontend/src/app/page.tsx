1e3864a7332260daac4f6bb9d9b1294ee3767f5b
ismail-arame <ismail.arame66@gmail.com>
2023-07-21 15:01:18 +0100

    added chat slice and get Conversations async thunk

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,23 +1,21 @@
 "use client";
 
 import { Sidebar } from "@/components/sidebar";
-import { logout } from "@/redux/features/userSlice";
+import { getConversations } from "@/redux/features/chatSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import axios from "axios";
-import Cookies from "js-cookie";
-import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const dispatch = useAppDispatch();
-  const router = useRouter();
   const { user } = useAppSelector((state) => state.user);
   console.log("user ===> ", user);
-  const handleLogout = async () => {
-    dispatch(logout());
-    Cookies.remove("usertoken");
-    await axios.post(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/auth/logout`);
-    router.push("/login");
-  };
+
+  //Get Conversations
+  useEffect(() => {
+    if (user?.access_token) {
+      dispatch(getConversations(user.access_token));
+    }
+  }, [user]);
   return (
     <div className="min-h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden">
       {/* Container */}
@@ -28,3 +26,15 @@ export default function Home() {
     </div>
   );
 }
+
+// import { logout } from "@/redux/features/userSlice";
+// import axios from "axios";
+// import Cookies from "js-cookie";
+// import { useRouter } from "next/navigation";
+// const router = useRouter();
+// const handleLogout = async () => {
+//   dispatch(logout());
+//   Cookies.remove("usertoken");
+//   await axios.post(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/auth/logout`);
+//   router.push("/login");
+// };
--- frontend/src/redux/features/chatSlice.ts
+++ frontend/src/redux/features/chatSlice.ts
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   status: "",
@@ -8,6 +9,26 @@ const initialState = {
   notifications: [],
 };
 
+//functions
+export const getConversations = createAsyncThunk(
+  "conversation/all",
+  async (token: string, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_ENDPOINT}/conversation`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return data;
+    } catch (error: any) {
+      return rejectWithValue(error.response.data.error.message);
+    }
+  }
+);
+
 export const chatSlice = createSlice({
   name: "chat",
   initialState,
@@ -15,6 +36,20 @@ export const chatSlice = createSlice({
     setActiveConversation: (state, action) => {
       state.activeConversation = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getConversations.pending, (state, action) => {
+      state.status = "loading";
+    });
+    builder.addCase(getConversations.fulfilled, (state, action) => {
+      state.status = "succeeded";
+      state.error = "";
+      state.conversations = action.payload;
+    });
+    builder.addCase(getConversations.rejected, (state, action) => {
+      state.status = "failed";
+      state.error = action.payload as any;
+    });
   },
 });
 
